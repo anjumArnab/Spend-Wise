68be4727e6bbf3cb6e16ea472cca5822e520d8fe
anjumArnab <anjumarnab050@gmail.com>
2025-02-03 19:32:53 +0600

    minor changes done

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:spend_wise/screens/homepage.dart';
 import 'package:spend_wise/services/firbase_auth_methods.dart';
