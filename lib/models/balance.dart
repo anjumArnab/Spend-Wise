b3e62cc113aa8b607b7d27890f214fbc755bc97d
anjumArnab <anjumarnab050@gmail.com>
2025-02-05 00:06:43 +0600

    data model added

---
+++ lib/models/balance.dart
@@ -0,0 +1,13 @@
+class Balance {
+  final double balance;
+  final double income;
+  final double expense;
+  final double value; // for linear progress indicator
+
+  Balance({
+    required this.balance,
+    required this.income,
+    required this.expense,
+    required this.value,
+  });
+}
