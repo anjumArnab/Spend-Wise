70f3882315852560d30d714b97af116f9c2f08ad
anjumArnab <anjumarnab050@gmail.com>
2025-04-03 23:24:56 +0600

    budget model

---
+++ lib/models/budget.dart
@@ -0,0 +1,34 @@
+class Budget {
+  int id;
+  String category;
+  double amount;
+  String startDate;
+  String endDate;
+
+  Budget({
+    required this.id,
+    required this.category,
+    required this.amount,
+    required this.startDate,
+    required this.endDate,
+  });
+  factory Budget.fromJson(Map<String, dynamic> json) {
+    return Budget(
+      id: json['id'],
+      category: json['category'],
+      amount: json['amount'].toDouble(),
+      startDate: json['start_date'],
+      endDate: json['end_date'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'category': category,
+      'amount': amount,
+      'start_date': startDate,
+      'end_date': endDate,
+    };
+  }
+}
