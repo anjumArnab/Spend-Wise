560be48f0ac8142b5a19a26fdc9ac6c634de04bb
anjumArnab <anjumarnab050@gmail.com>
2025-04-09 23:39:56 +0600

    user info form

--- lib/screens/user_info_form.dart
+++ lib/screens/user_info_form.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:spend_wise/models/user.dart';
+import 'package:spend_wise/services/authentication.dart';
+import 'package:spend_wise/services/cloud_store.dart';
 import 'package:spend_wise/widgets/border_button.dart';
 import 'package:spend_wise/widgets/custom_text_field.dart';
+import 'package:spend_wise/widgets/show_snack_bar.dart';
 
 class UserInfoForm extends StatefulWidget {
   //final UserModel userData; // Accept user data from UserDetailsPage
@@ -13,6 +17,9 @@ class UserInfoForm extends StatefulWidget {
 class _UserInfoFormState extends State<UserInfoForm> {
   final TextEditingController _fullname = TextEditingController();
   final TextEditingController _phone = TextEditingController();
+
+  final FirebaseAuthService _authService = FirebaseAuthService();
+  final FirestoreService _db = FirestoreService();
 
   // Define static lists for dropdown items
   static const List<String> genderOptions = ['Male', 'Female', 'Other'];
@@ -32,7 +39,7 @@ class _UserInfoFormState extends State<UserInfoForm> {
     'Japanese'
   ];
 
-String? _selectedGender;
+  String? _selectedGender;
   String? _selectedBloodGroup;
   String? _selectedLanguage;
   // Helper method to create dropdown buttons with consistent styling
@@ -55,6 +62,49 @@ String? _selectedGender;
     );
   }
 
+  // Function to save user information
+
+  Future<void> _saveUserInfo() async {
+    // Add validation to ensure all required dropdowns are selected
+    if (_selectedGender == null ||
+        _selectedBloodGroup == null ||
+        _selectedLanguage == null) {
+      showSnackBar(context, 'Please select all dropdown values');
+      return;
+    }
+
+    // Validate required text fields
+    if (_fullname.text.isEmpty || _phone.text.isEmpty) {
+      showSnackBar(context, 'Please fill in all required fields');
+      return;
+    }
+    try {
+      UserModel user = UserModel(
+        uid: _authService.currentUser!.uid,
+        email: _authService.currentUser!.email!,
+        fullName: _fullname.text,
+        gender: _selectedGender!,
+        bloodGroup: _selectedBloodGroup!,
+        preferredLanguage: _selectedLanguage!,
+      );
+
+      await _db.saveUserData(user, _authService.currentUser!.uid);
+    } catch (e) {
+      showSnackBar(context, 'Error saving user data: ${e.toString()}');
+    } finally {
+      // Clear the text fields and dropdowns after saving
+      _fullname.clear();
+      _phone.clear();
+      setState(() {
+        _selectedGender = null;
+        _selectedBloodGroup = null;
+        _selectedLanguage = null;
+      });
+      showSnackBar(context, 'User information saved successfully!');
+      // Optionally,  navigate to another screen or show a success message
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,7 +121,7 @@ String? _selectedGender;
                 'Please enter information',
                 style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
               ),
-              Text('for '), // ${_authService.currentUser!.email}
+              Text('for ${_authService.currentUser!.email}'),
               const SizedBox(height: 20),
 
               // Personal Information Section
@@ -89,8 +139,6 @@ String? _selectedGender;
                 keyboardType: TextInputType.text,
               ),
               const SizedBox(height: 15),
-
-            
 
               // Dropdown Row
               Wrap(
@@ -110,7 +158,7 @@ String? _selectedGender;
                             _selectedGender = newValue;
                           });
                         },
-                        hint: 'Select Gender',
+                        hint: 'Gender',
                       ),
                     ],
                   ),
@@ -128,7 +176,7 @@ String? _selectedGender;
                             _selectedBloodGroup = newValue;
                           });
                         },
-                        hint: 'Select Blood Group',
+                        hint: 'Blood Group',
                       ),
                     ],
                   ),
@@ -146,7 +194,7 @@ String? _selectedGender;
                             _selectedLanguage = newValue;
                           });
                         },
-                        hint: 'Select Language',
+                        hint: 'Language',
                       ),
                     ],
                   ),
@@ -158,7 +206,7 @@ String? _selectedGender;
               // Contact Information Section
               const Text(
                 'Contact Information',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
               ),
               const Divider(),
               const SizedBox(height: 10),
@@ -171,14 +219,9 @@ String? _selectedGender;
               ),
               const SizedBox(height: 15),
 
-              
-
-              
-
-      
               BorderButton(
                 text: 'Save',
-                onPressed: (){},
+                onPressed: _saveUserInfo,
               ),
               const SizedBox(height: 20),
             ],
