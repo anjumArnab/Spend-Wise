50a93d85e43bc90c68eb5f93a680e3e2a29ff967
anjumArnab <anjumarnab050@gmail.com>
2025-04-12 20:23:37 +0600

    final VoidCallback? onVoidPressed; and final Function()? onPressed

--- lib/widgets/border_button.dart
+++ lib/widgets/border_button.dart
@@ -2,18 +2,20 @@ import 'package:flutter/material.dart';
 
 class BorderButton extends StatelessWidget {
   final String text;
-  final VoidCallback onPressed;
+  final VoidCallback? onVoidPressed;
+  final Function()? onPressed;
 
   const BorderButton({
     super.key,
     required this.text,
-    required this.onPressed,
+    this.onVoidPressed,
+    this.onPressed
   });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onPressed,
+      onPressed: onVoidPressed ?? onPressed,
       style: ElevatedButton.styleFrom(
         minimumSize: const Size(double.infinity, 50),
         shape: RoundedRectangleBorder(
