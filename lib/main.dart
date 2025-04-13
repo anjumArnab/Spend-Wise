af949c03ea861b4ce0d4d76db247298f8f41549d
anjumArnab <anjumarnab050@gmail.com>
2025-04-13 21:41:04 +0600

    homepage > signupscreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:spend_wise/firebase_options.dart';
-import 'package:spend_wise/screens/homepage.dart';
+import 'package:spend_wise/screens/sign_up_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +23,7 @@ class SpendWise extends StatelessWidget {
         textTheme: GoogleFonts.poppinsTextTheme()
       ),
       title: 'Spend Wise',
-      home: const HomePage(),
+      home: const SignUpScreen(),
     );
   }
 }
